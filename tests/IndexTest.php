e306fc21f6132436b63ce5754bd0407146282814
Dmitry Lukashin <dlukashin@gmail.com>
2017-09-02 03:59:33 +0300

    Remove excessive tests

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -5,17 +5,10 @@ class IndexTest extends \PHPUnit\Framework\TestCase
     public function correctValues()
     {
         return [
-            'zeros' => [0, 0, 0],
-            'all positive' => [2, 3, 5],
-            'one negative' => [-7, 4, -3],
-            'all negative' => [-7, -4, -11],
-        ];
-    }
-
-    public function incorrectValues()
-    {
-        return [
-            [1, -1, 1]
+            [0, 0, 0],
+            [2, 3, 5],
+            [-7, 4, -3],
+            [-7, -4, -11],
         ];
     }
 
@@ -25,13 +18,5 @@ class IndexTest extends \PHPUnit\Framework\TestCase
     public function testCorrectSum($a, $b, $sum)
     {
         $this->assertEquals($sum, (new Index)->sum($a, $b));
-    }
-
-    /**
-     * @dataProvider incorrectValues
-     */
-    public function testWrongSum($a, $b, $sum)
-    {
-        $this->assertNotEquals($sum, (new Index)->sum($a, $b));
     }
 }
